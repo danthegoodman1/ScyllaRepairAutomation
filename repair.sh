5d8d5b755dc6f8df57cd4f7f4345ce6dc5a9ca33
Dan Goodman <xxdanthegoodmanxx@gmail.com>
2023-06-23 13:20:05 -0400

    add -n

--- repair.sh
+++ repair.sh
@@ -1,7 +1,7 @@
 IFS=',' read -ra HOSTS <<< "$1" # you could replace $1 with a hard-coded list if you prefer
 for i in "${HOSTS[@]}"; do
   echo "🛠️  Repairing host '$i'..."
-  output=$(ssh root@$i -C "nodetool repair -pr") # replace with your command (ssh, kubectl, fly ssh, etc.)
+  output=$(ssh -n root@$i -C "nodetool repair -pr") # replace with your command (ssh, kubectl, fly ssh, etc.)
   exit_code=$?
   if [ $exit_code -ne 0 ]; then
       echo "😥 SSH command failed with exit code $exit_code for host '$i'"
